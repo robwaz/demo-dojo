327956e838712ec2d950c9dc6cb225641b98723b
frqmod <me@frqmod.dev>
2025-08-26 16:01:09 -0700

    oops

--- cse466-demos/stack-corruption-demo/stackdemo.c
+++ cse466-demos/stack-corruption-demo/stackdemo.c
@@ -3,10 +3,17 @@
 #include <stdint.h>
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input_string>\n", argv[0]);
+        return 1;
+    }
+
     uint64_t secret = 0x1122334455667788ULL;
     char     buf[8];
 
     strcpy(buf, argv[1]);
+
+    printf("The value of secret is: 0x%016lx\n", secret);
 
     return 0;
 }
